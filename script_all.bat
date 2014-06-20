77a6093fbe3de537981fec0631d969d6ae3b1d6f
Emmanuel Orvain-PCOffice <emmanuelx.orvain@intel.com>
2014-06-20 16:16:13 +0200

    update/remove scripts

--- script_all.bat
+++ script_all.bat
@@ -1,5 +1,36 @@
 ECHO OFF
-ECHO SEPIA BATCH CALL
+
+ECHO BGR2GRAY BATCH CALL
+CALL script_rgb2gray.bat %1
+
+ECHO BORDER BATCH CALL
+CALL script_border.bat %1
+
+ECHO SEPIA-TONE BATCH CALL
 CALL script_sepia.bat %1
+
+ECHO SEPIA-MATRIX BATCH CALL
+CALL script_sepia_matrix.bat %1
+
+ECHO POLAROID BATCH CALL
+CALL script_polaroid_matrix.bat %1
+
+ECHO SEPIA BATCH CALL
+CALL script_sepia_matrix.bat %1
+
 ECHO VIGNETTE BATCH CALL
 CALL script_vignette.bat %1-sepia
+
+ECHO PHOTOBOOTH BATCH CALL
+CALL script_photobooth_4x4.bat test 20 20
+
+ECHO RESIZE WISITIT LOGO BATCH CALL
+CALL script_resize_wistiti.bat
+
+ECHO WATERMARK BATCH CALL
+CALL script_watermark.bat test
+
+
+
+
+
