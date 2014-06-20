baaaa23b120a1cc7f05ef7429777286189a27927
Emmanuel Orvain-PCOffice <emmanuelx.orvain@intel.com>
2014-06-20 16:16:48 +0200

    add watermark script

---
+++ script_watermark.bat
@@ -0,0 +1,6 @@
+echo OFF
+echo set "test" as parameter for example
+echo input file : wistiti_1024.png
+echo processing...
+composite -gravity SouthEast wistiti-resized.png %1.png %1-watermark.png
+echo output file : %1-watermark.png
