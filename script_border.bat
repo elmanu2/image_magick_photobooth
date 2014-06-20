acbfeb88bdf74e1276cd0dd9f0f6d7b0a260d0cc
Emmanuel Orvain-PCOffice <emmanuelx.orvain@intel.com>
2014-06-20 15:58:47 +0200

    add new scripts border, photobooth, rgb2gray, wistiti_logo

---
+++ script_border.bat
@@ -0,0 +1,6 @@
+echo OFF
+echo set "test" as parameter for example
+echo input file : %1.png
+echo processing...
+convert %1.png -bordercolor black -border 10x10  %1-border.png
+echo output file : %1-border.png
---
+++ script_rgb2gray.bat
@@ -0,0 +1,7 @@
+echo OFF
+echo set "test" as parameter for example
+echo add vignette filter to an image and save into another file
+echo input file : %1.png
+echo processing...
+convert %1.jpg -colorspace Gray %1_gray.png
+echo output file : %1-gray.png
