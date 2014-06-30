39a814279fbcd5815ee3ee184a4304986e2abfe5
manu <manu@manu-Inspiron-1525.(none)>
2014-06-30 21:10:29 +0200

    add vintage effect for linux

--- node_scripts/main.js
+++ node_scripts/main.js
@@ -9,10 +9,12 @@ process.chdir('..');
 var project_dir = process.cwd();
 var input_pic_dir = project_dir + '/input_pictures/';
 //var script_dir = project_dir + '/im_scripts/bash/';
-if(process.platform == "win32"){
+var platform = process.platform;
+
+if(platform == "win32"){
     var script_dir = project_dir + '/im_scripts/batch/';
 }
-else{
+if(platform == 'linux'){
     var script_dir = project_dir + '/im_scripts/bash/';
 }
 
@@ -51,28 +53,37 @@ function pic_size(filepath, callback){
 
 console.log(process.platform);
 
-if(process.platform == "win32"){
+if(platform == 'win32'){
     process_filter(ex_nihilo_process, './script_create_checkboard.bat');
     //process_filter(simple_process_with_sizing, './script_vintage.bat', 'test');
     
 }
-else{
+if(platform == 'linux'){
     process_filter(ex_nihilo_process, './script_create_checkboard.sh');
     //process_filter(simple_process, './script_rgb2gray.sh','test');
     //process_filter(simple_process, './script_polaroid_matrix.sh','test');
     //process_filter(simple_process, './script_sepia.sh','test');
     //process_filter(simple_process, './script_sepia_matrix.sh','test');
     //process_filter(border_process, './script_border.sh','test', "20", "10");
-    //process_filter(simple_process_with_sizing, './script_vintage.sh', 'test');
+    process_filter(simple_process_with_sizing, './script_vintage.sh','sf-bridge', 0, 0,
+        function(){
+            process_filter(simple_process_with_sizing, './script_vintage.sh', 'test', 0, 0,
+                function(){
+                //do nothing
+            });
+
+        });
 }
 
-function process_filter(process_type, process_name, file, width, height){
+function process_filter(process_type, process_name, file, width, height, cb){
     var node_script_directory = process.cwd();
     process.chdir(script_dir);
-    console.log(process.cwd());
-    process_type(process_name, file, width, height);
-    process.chdir(node_script_directory);
-    console.log(process.cwd());
+    console.log('change cwd : ' + process.cwd());
+    process_type(process_name, file, width, height, function(){
+        process.chdir(node_script_directory);
+        console.log('change cwd : ' + process.cwd());
+        cb();
+    });
 }
 
 function ex_nihilo_process(process_name){
@@ -96,17 +107,19 @@ function border_process(process_name, file, width, height){
     });
 }
 
-function simple_process_with_sizing(process_name, file){
+function simple_process_with_sizing(process_name, file, p1, p2, cb){
     pic_size(input_pic_dir + file + '.png', function(size){
         
         console.log('after size : ' + size.width + 'x' + size.height);
         console.log("process_name : " + process_name);
         console.log("file : " + file);
-        
+        console.log("process name : " + process_name);
+        console.log('cwd : ' + process.cwd());
         process_cmd = spawn(process_name, [file, size.width, size.height]);
         //process_cmd.stdout.on('data', function(data){
         //    console.log('stdout: ' + data);
         //});
+        cb();
     });
 }
 
