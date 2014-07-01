d8386e136ccdf55379af7b98de491396c3eb7fdb
manu <manu@manu-Inspiron-1525.(none)>
2014-07-01 21:47:30 +0200

    fit script for linux environment and run tests

--- python_scripts/image_processing.py
+++ python_scripts/image_processing.py
@@ -6,15 +6,19 @@ from PIL import Image
 def get_image_size(filepath):
     print(filepath)
     im=Image.open(filepath)
-    im.size # (width,height) tuple
-
+    return im.size # (width,height) tuple
+    
 
 py_dir = os.getcwd();
 os.chdir('..');
 project_dir = os.getcwd();
-in_pic_dir = project_dir + '\\input_pictures\\';
-out_pic_dir = project_dir + '\\output_pictures\\';
-script_dir = project_dir + '\\im_scripts\\bash\\';
+
+#in_pic_dir = project_dir + '\\input_pictures\\';
+#out_pic_dir = project_dir + '\\output_pictures\\';
+#script_dir = project_dir + '\\im_scripts\\bash\\';
+in_pic_dir = project_dir + '/input_pictures/';
+out_pic_dir = project_dir + '/output_pictures/';
+script_dir = project_dir + '/im_scripts/bash/';
 
 print(py_dir);
 print(project_dir);
@@ -23,9 +27,29 @@ print(out_pic_dir);
 print(script_dir);
 
 os.chdir(script_dir);
-subprocess.call('sh script_create_checkboard.sh');
+print("cwd %s" %(os.getcwd()));
 
-file = in_pic_dir + 'test.pn';
+input_pic = 'test';
 
-get_image_size(file);
+#WINDOWS
+#subprocess.call('sh script_create_checkboard.sh');
+#subprocess.call('./script_border.sh 10 20');
+
+#LINUX
+subprocess.call('./script_create_checkboard.sh');
+subprocess.call('./script_border.sh %s 10 10' %input_pic, shell=True);
+subprocess.call('./script_frame.sh %s' %input_pic, shell=True);
+subprocess.call('./script_photobooth_4x4.sh %s' %input_pic, shell=True);
+subprocess.call('./script_polaroid_matrix.sh %s' %input_pic, shell=True);
+subprocess.call('./script_sepia_matrix.sh %s' %input_pic, shell=True);
+subprocess.call('./script_sepia.sh %s' %input_pic, shell=True);
+subprocess.call('./script_resize_wistiti.sh', shell=True);
+subprocess.call('./script_watermark.sh %s' %input_pic, shell=True);
+
+
+
+file = in_pic_dir + 'test.png';
+pic_size = get_image_size(file);
+subprocess.call('./script_vintage.sh %s %d %d' %(input_pic, pic_size[0],pic_size[1]), shell=True);
+
 
